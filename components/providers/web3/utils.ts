fab34226d824c187ed54d4c18b3c1718bf32f125
RonakPotha <83223271+RonakPotha@users.noreply.github.com>
2023-01-02 20:13:16 +0530

    use acc abs

--- components/providers/web3/utils.ts
+++ components/providers/web3/utils.ts
@@ -1,5 +1,6 @@
 import { setupHooks, Web3Hooks } from "@hooks/web3/setupHooks";
 import { MetaMaskInpageProvider } from "@metamask/providers";
+import { Web3Dependencies } from "@_types/hooks";
 import { Contract, ethers, providers } from "ethers";
 
 declare global {
@@ -8,16 +9,15 @@ declare global {
   }
 }
 
-export type Web3Params = {
-  ethereum: MetaMaskInpageProvider | null;
-  provider: providers.Web3Provider | null;
-  contract: Contract | null;
+type Nullable<T> = {
+  [P in keyof T]: T[P] | null;
 }
 
 export type Web3State = {
   isLoading: boolean; // true while loading web3State
-  hooks: Web3Hooks
-} & Web3Params
+  hooks: Web3Hooks;
+} & Nullable<Web3Dependencies>
+
 
 export const createDefaultState = () => {
   return {
@@ -29,25 +29,30 @@ export const createDefaultState = () => {
   }
 }
 
+export const createWeb3State = ({
+  ethereum, provider, contract, isLoading
+}: Web3Dependencies & {isLoading: boolean}) => {
+  return {
+    ethereum,
+    provider,
+    contract,
+    isLoading,
+    hooks: setupHooks({ethereum, provider, contract})
+  }
+}
+
 const NETWORK_ID = process.env.NEXT_PUBLIC_NETWORK_ID;
 
 export const loadContract = async (
   name: string,  // NftMarket
   provider: providers.Web3Provider
 ): Promise<Contract> => {
-  
-    
-
   if (!NETWORK_ID) {
     return Promise.reject("Network ID is not defined!");
   }
 
- 
-
   const res = await fetch(`/contracts/${name}.json`);
   const Artifact = await res.json();
-
-  
 
   if (Artifact.networks[NETWORK_ID].address) {
     const contract = new ethers.Contract(
@@ -55,8 +60,6 @@ export const loadContract = async (
       Artifact.abi,
       provider
     )
-
-
 
     return contract;
   } else {
